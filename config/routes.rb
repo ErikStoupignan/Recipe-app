3c068de22fb522d89cbc398c4338d4b176455359
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-12-14 08:43:45 +0100

    Create routes for food, recipe and setup Controller for food

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   root 'users#index'
+  resources :foods, only: [:index]
+  resources :recipes, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    resources :recipe_foods, only: [:new, :create, :destroy]
+  end
 
   resources :users, only: [:index, :show] do
   end
