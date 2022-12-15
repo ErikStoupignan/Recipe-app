3407d51db7348a47f641da244013ad2b2cb24a3b
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-12-15 14:16:00 -0600

    Fix problem to add ingredientes into recipes

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,9 +1,9 @@
 class RecipeFoodsController < ApplicationController
   # before_action :authenticate_user!, :set_recipe_food, only: %i[show edit update destroy]
 
-  def index
-    @foods = Food.includes([:food])
-  end
+  # def index
+  #   @foods = Food.includes([:food])
+  # end
 
   def new
     @recipe = Recipe.find(params[:recipe_id])
@@ -15,8 +15,9 @@ class RecipeFoodsController < ApplicationController
     @recipe_food = @recipe.recipe_foods.create(recipe_foods_params)
 
     if @recipe_food.save
-      redirect_to @recipe
-      render :new, alert: 'Food created sucessfully.'
+      # redirect_to @recipe
+      render :new, notice: 'Food created sucessfully.'
+
     else
       render :new, notice: 'Failed to add food'
     end
@@ -24,7 +25,6 @@ class RecipeFoodsController < ApplicationController
 
 
   private
-
 
   def recipe_foods_params
     params.require(:recipe_food).permit(:quantity, :food_id)
